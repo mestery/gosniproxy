d258d3ed9631e927e4fcc22e2d7979c6a1a41ffa
Kyle Mestery <mestery@mestery.com>
2026-03-02 16:00:48 -0600

    eBPF fixes
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- build-ebpf.sh
+++ build-ebpf.sh
@@ -6,6 +6,6 @@ mkdir -p build
 cd kern
 
 echo "Building eBPF program..."
-clang -O2 -target bpf -c sockmap.bpf.c -o ../build/sockmap.bpf.o
+clang -O2 -target bpf -c proxy_kern.bpf.c -o ../build/proxy_kern.bpf.o
 
-echo "eBPF program built successfully to build/sockmap.bpf.o"
+echo "eBPF program built successfully to build/proxy_kern.bpf.o"
